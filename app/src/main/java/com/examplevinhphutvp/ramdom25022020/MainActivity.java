e8f18b26d92d7234ceadfa09e35d700b468190c1
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-03-15 00:56:11 +0700

    Math.random

--- app/src/main/java/com/examplevinhphutvp/ramdom25022020/MainActivity.java
+++ app/src/main/java/com/examplevinhphutvp/ramdom25022020/MainActivity.java
@@ -13,13 +13,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Math
         // Lam tron
-        Double a =1.99;
-        long value = Math.round(a);
-        Log.d("BBB",String.valueOf(a));
+ //       Double a =1.99;
+ //       long value = Math.round(a);
+ //       Log.d("BBB",String.valueOf(a));
         // Lam tron len
-        double value1 = Math.ceil(a);
+ //       double value1 = Math.ceil(a);
         // lam tron xuong
-        double value2 = Math.floor(a);
-        Log.d("BBB",String.valueOf(value2));
+ //       double value2 = Math.floor(a);
+ //       Log.d("BBB",String.valueOf(value2));
+        // can hai
+ //       double b = 9;
+ //       double vaLue = Math.sqrt(b);
+ //       Log.d("BBB",String.valueOf(vaLue));
+        // tinh so max va min
+ //       int a = 5;
+ //       int b = 6;
+ //       int value = Math.max(a,b);
+ //       Log.d("BBB",String.valueOf(value));
+        double value = Math.floor(Math.random() * 6);
+        Log.d("BBB",String.valueOf(value));
     }
 }
