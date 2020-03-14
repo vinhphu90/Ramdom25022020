e1063d8cb9cf717df01d3e814ddfa284cd836fd2
vinhphu90 <61481544+vinhphu90@users.noreply.github.com>
2020-03-15 00:10:17 +0700

    lam tron

--- app/src/main/java/com/examplevinhphutvp/ramdom25022020/MainActivity.java
+++ app/src/main/java/com/examplevinhphutvp/ramdom25022020/MainActivity.java
@@ -3,6 +3,7 @@ package com.examplevinhphutvp.ramdom25022020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Math
+        // Lam tron
+        Double a =1.99;
+        long value = Math.round(a);
+        Log.d("BBB",String.valueOf(a));
+        // Lam tron len
+        double value1 = Math.ceil(a);
+        // lam tron xuong
+        double value2 = Math.floor(a);
+        Log.d("BBB",String.valueOf(value2));
     }
 }
